555279e18e2dd6c78eec1b3ae33ebbd3100dffbd
arash <a_massumi@comp.iust.ac.ir>
2020-08-17 17:46:29 +0430

    add_test_source

---
+++ test_source/1.cpp
@@ -0,0 +1,25 @@
+//in the name of allah
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int in;
+	cin>>in;
+	if (in >0){
+		{
+			cout<<"test";
+		}
+		if(in>9)
+	    	cout<<"ya_allah";
+	    else
+	    	in =10;
+	    if (in<10)
+	    	cout<<"ya_ali";
+	}
+	else
+	    cout<<"ya_ali";
+	(in > 1) ? cout<<"ya_zahra" : cout<<"ya_ali";
+	return 0;
+}
