12f0e5ed3bce2cf62fe763863403ae3b654f2ecc
m-zakeri <m-zakeri@live.com>
2021-11-06 12:39:04 +0330

    Update code fragments for tests

--- test_source/1.cpp
+++ test_source/1.cpp
@@ -2,11 +2,44 @@
 #include<iostream>
 using namespace std;
 
+/*  */
 int main()
 {
 	int in;
-	if (in >0){
-	    cout<<0;
+	in = 1;
+	if (in > 0)
+	{
+	    cout << "In is not Zero!";
 	}
 	return 0;
+}
+
+int f1()
+{
+     int i=1;
+     int j=10;
+     for (i=1; i<=j; i++)
+     {
+          print(i);
+     }
+
+    return i;
+}
+
+
+int f2()
+{
+     int i=1;
+     int j=10;
+     while (i<=j)
+     {
+          if (!i%j)
+          {
+                cout << "i enumerate j";
+          }
+          print(i);
+          i++;
+     }
+
+    return i;
 }
