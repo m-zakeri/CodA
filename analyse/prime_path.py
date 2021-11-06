b1578b134cf9b31922d8d72f3052db15268d06aa
m-zakeri <m-zakeri@live.com>
2021-11-06 12:24:22 +0330

    rename module and class refactorings

--- analyse/prime_path.py
+++ analyse/prime_path.py
@@ -1,10 +1,11 @@
-#in the name of allah
+"""
+The Graph class in the module computes prime paths
 
-#graph class for compute prime paths
+"""
 
-class Graph:
 
-    def __init__(self , graph_file):
+class ControlFlowGraph:
+    def __init__(self, graph_file):
         lines = graph_file.readlines()
         self.edges = {}
         self.nodes = set()
@@ -12,7 +13,7 @@ class Graph:
         self.finals = set()
         for i in range(len(lines) - 2):
             line = lines[i]
-            n , dest = (int(x) for x in line.split(' '))
+            n, dest = (int(x) for x in line.split(' '))
             self.nodes.add(dest)
             try:
                 self.edges[n].append(dest)
@@ -20,7 +21,7 @@ class Graph:
                 self.edges[n] = [dest]
 
         init_line = lines[-2]
-        init_nodes = set( int(x) for x in init_line.split(':')[1].split(' '))
+        init_nodes = set(int(x) for x in init_line.split(':')[1].split(' '))
         self.inits = init_nodes
         self.nodes.update(self.inits)
 
@@ -29,7 +30,7 @@ class Graph:
         self.finals = final_nodes
         self.nodes.update(self.finals)
 
-    def reachHead(self , path):
+    def reachHead(self, path):
         first = path[0]
         non_final_nodes = self.nodes - self.finals
         for n in non_final_nodes:
@@ -37,7 +38,7 @@ class Graph:
                 return False
         return True
 
-    def reachEnd(self , path):
+    def reachEnd(self, path):
         last = path[-1]
         if last in self.finals:
             return True
@@ -46,7 +47,7 @@ class Graph:
                 return False
         return True
 
-    def extend(self , path):
+    def extend(self, path):
         extended_paths = []
         last = path[-1]
         if last in self.finals:
@@ -55,20 +56,18 @@ class Graph:
             if n not in path[1:]:
                 extended_paths += [path + [n]]
         return extended_paths
-    
+
     def computePrimePaths(self):
         self.primePaths = []
         try_paths = [[n] for n in self.nodes]
         while len(try_paths) > 0:
             new_try_paths = []
             for path in try_paths:
-                if path[0] == path[-1] and len(path) >= 2: #prime path
+                if path[0] == path[-1] and len(path) >= 2:  # prime path
                     self.primePaths.append(path)
                 elif self.reachEnd(path):
-                    if self.reachHead(path): #prime path
+                    if self.reachHead(path):  # prime path
                         self.primePaths.append(path)
-                else: #extendable path
+                else:  # extendable path
                     new_try_paths += self.extend(path)
             try_paths = new_try_paths
-
-
