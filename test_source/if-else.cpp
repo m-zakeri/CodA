7f069da8e01ff59c028c4ca8d96097e4d714ecbb
Mojtaba Babayi <babayi.mojtaba.mb@gmail.com>
2021-11-21 15:49:26 +0330

    Add if-else sample

---
+++ test_source/if-else.cpp
@@ -0,0 +1,27 @@
+int main()
+{
+    int tr, fa;
+    int inner;
+    fa = 1;
+    if(a)
+    {
+        tr++;
+        if(b) c--;
+        tr--;
+    }
+    else fa++;
+    if(a > 2)
+    {
+        tr++;
+        tr--;
+        tr += 1;
+        if (tr)
+        {
+            inner++;
+            if (fa) fa--;
+            inner--;
+        }
+        fa += 2;
+    }
+    end++;
+}
--- test_visitor.py
+++ test_visitor.py
@@ -5,7 +5,7 @@ from gen.CPP14_v2Lexer import CPP14_v2Lexer
 from gen.CPP14_v2Parser import CPP14_v2Parser
 from graph_utils import draw_CFG
 
-file_name = "dowhile"
+file_name = "if-else"
 file_path = f'test_source/{file_name}.cpp'
 with open(file_path, 'r') as f:
     source = f.read()
